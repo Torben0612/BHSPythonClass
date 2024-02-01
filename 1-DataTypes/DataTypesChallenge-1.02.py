6a018cccbe52445446e22df30f287d7428d20dc8
Torben0612 <torben.krah@gmail.com>
2024-02-01 09:46:30 -0500

    chanlengedatatype-1.02

--- 1-DataTypes/DataTypesChallenge-1.02.py
+++ 1-DataTypes/DataTypesChallenge-1.02.py
@@ -1,9 +1,9 @@
 #Challenge  1: Four 4Fours
-print('zero is', (44+44))
+print('zero is', (4**4)*4%4)
 print('one is', (44/44))
 print('two is', (4/4)+4//4)
 print('three is', (4*4-4)//4)
-#print('four is', (44+4))
+print('four is', (4**-4)//4+4)
 
 
 #Challenge 2: Printing Two Lines
