b277b67d553443b2ee1cc1b473955e2d182202cd
Torben0612 <torben.krah@gmail.com>
2024-02-02 09:13:39 -0500

    update

---
+++ 1-DataTypes/PrintingLab1.03.py
@@ -0,0 +1,4 @@
+favoriteNumber = int(input("what is your fav number")) #youpick
+birthdayMonth = int(input("what is your birth day month enter 1 for jan 2 for feb, ect"))#youFillitIn
+luckyNumber = birthdayMonth + favoriteNumber #sum of your favorite number and birthday month
+print("Your lucky number is " + str(luckyNumber))
--- 1-DataTypes/datatype1.03.py
+++ 1-DataTypes/datatype1.03.py
@@ -1,1 +1,12 @@
-print()
+print("Hello\n\nWorld")
+print("\n")
+
+print("Hello\n" + "\nWorld")
+print("\n")
+
+print("Hello" + "\n\n" +"World")
+print("\n")
+
+print("Hello")
+print("\n")
+print("World")
