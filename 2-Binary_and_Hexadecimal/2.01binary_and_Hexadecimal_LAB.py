686d79131be43b1b0fb43502330f94fa599c9855
Torben0612 <74421064+Torben0612@users.noreply.github.com>
2024-02-27 07:35:14 -0500

    Update 2.01binary_and_Hexadecimal_LAB.py

--- 2-Binary_and_Hexadecimal/2.01binary_and_Hexadecimal_LAB.py
+++ 2-Binary_and_Hexadecimal/2.01binary_and_Hexadecimal_LAB.py
@@ -1,2 +1,7 @@
 #Part 4
-print(("T", bin("T"),ord("T")))
+print("T", "0b1010100", ord("T"))
+print("O", "0b1001101", ord("O"))
+print("R", "0b1010010", ord("R"))
+print("B", "0b1000010", ord("B"))
+print("E", "0b1000101", ord("E"))
+print("N", "0b1001110", ord("N"))
