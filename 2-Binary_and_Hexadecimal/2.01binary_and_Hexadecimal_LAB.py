33df1c37d4e4ca3bf2f87213dfe6dc443df6acc2
Torben0612 <torben.krah@gmail.com>
2024-02-28 08:46:36 -0500

    update stuff

--- 2-Binary_and_Hexadecimal/2.01binary_and_Hexadecimal_LAB.py
+++ 2-Binary_and_Hexadecimal/2.01binary_and_Hexadecimal_LAB.py
@@ -5,3 +5,6 @@ print("R", "0b1010010", ord("R"))
 print("B", "0b1000010", ord("B"))
 print("E", "0b1000101", ord("E"))
 print("N", "0b1001110", ord("N"))
+
+#part 5
+print("\U0001F441", "\U0001F497", "\U0001F408", "and", "\U0001F3AE")
