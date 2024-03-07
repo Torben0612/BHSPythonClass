afec84ea0eb237ea5986bc6b711d2a0704851096
Torben0612 <74421064+Torben0612@users.noreply.github.com>
2024-03-07 08:53:42 -0500

    Update 2.02-Boolean-LAB.py

--- 2-Binary_and_Hexadecimal/2.02-Boolean-LAB.py
+++ 2-Binary_and_Hexadecimal/2.02-Boolean-LAB.py
@@ -1,11 +1,1 @@
-#Part II
 
-#Part III
-print("Give me your height by giving total feed and then on the next prompt the remaining inches")
-heightfeet = int(input("How many feet tall are you? "))
-heightinch = int(input("How many additional inches"))
-
-heightfeet * 12
-totalheight = heightfeet + heightinch
-totalheight == 5 
-print("You are tall enough to ride the rolar coaster:", totalheight == 50 or totalheight > 50)
