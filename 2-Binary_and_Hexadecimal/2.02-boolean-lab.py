f6747198cc3c28d16197f62221dcd4b3c3010cce
Torben0612 <24krahnstoevert@brooklinek12.org>
2024-03-07 08:49:10 -0500

    add code

---
+++ 2-Binary_and_Hexadecimal/2.02-boolean-lab.py
@@ -0,0 +1,9 @@
+age = int(input("How old are you?"))
+ageok = age >= 36
+print("You are old enough to be President:", ageok)
+
+yearslived = int(input("How many years have you lived in us?"))
+yearslivedok = yearslived >= 14
+print("You have been a resident for at least 14 years:", yearslivedok)
+
+natborn = 
